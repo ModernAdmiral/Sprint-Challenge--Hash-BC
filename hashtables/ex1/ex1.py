b421151f560f87c3bbf880b488d855640015700b
Jackson McComas <jackson.mccomas@gmail.com>
2020-04-10 13:05:10 -0500

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,6 +13,17 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
+    for i in range(length):
+        key = limit - weights[i]
+        print("weights[i]", weights[i])
+        print("limit", limit)
+        print("key", key)
+        pair_index = hash_table_retrieve(ht, key)
+
+        if pair_index is None:
+            hash_table_insert(ht, weights[i], i)
+        else:
+            return(i, pair_index)
     return None
 
 
